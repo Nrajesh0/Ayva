478cd3da98e4da2b5f0bc92c18c4829a08ffff50
Nrajesh0 <superk2537@gmail.com>
2026-09-21 00:49:14 +0530

    chore: bump version to 1.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     multiDexEnabled = true
     minSdk = 24
     targetSdk = 35
-    versionCode = 90
-    versionName = "1.9.0"
+    versionCode = 92
+    versionName = "1.9.2"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
